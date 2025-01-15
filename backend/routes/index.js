2f5eb36eaf1cac6b72a409527626f2d81bfdc337
rcodron-git <robert.codron@outlook.com>
2025-01-15 13:41:56 +0100

    fix error on index

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,8 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  //res.render('index', { title: 'Express' });
-  res.send("Welcome to the App!");
+
+
+  res.json({"message": "Welcome to the App!"});
+  
 });
 
 module.exports = router;
